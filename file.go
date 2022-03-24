1f1dcaa3b0c0c858f1df1983ecc0e6e85b40e18e
Christian Engel <engel-ch@outlook.com>
2022-03-24 11:49:02 +0100

    Calls split into ByteArray2File and ByteArray2ReponseWriter

--- file.go
+++ file.go
@@ -2,6 +2,7 @@ package go_libs
 
 import (
 	"errors"
+	"net/http"
 	"os"
 	"path/filepath"
 )
@@ -11,9 +12,24 @@ func FilenameWithoutSuffix(filename string) string {
 	return filename[0 : len(filename)-len(extension)]
 }
 
-// WriteByteArray writes a byte array into a file. If required it does so in multiple steps.
+// ByteArray2File writes a byte array into a file. If required it does so in multiple steps.
 // If all succeeds then nil is returned, otherwise an error.
-func WriteByteArray(file *os.File, bytes []byte) error {
+func ByteArray2File(file *os.File, bytes []byte) error {
+	nsum := 0
+	n := 0
+	var err error
+	for ; nsum < len(bytes); nsum += n {
+		n, err = file.Write(bytes[nsum:])
+		if err != nil { // should also never happen
+			return errors.New(CurrentFunctionName() + ":" + err.Error())
+		}
+	}
+	return nil
+}
+
+// ByteArray2ReponseWriter writes a byte array into a file. If required it does so in multiple steps.
+// If all succeeds then nil is returned, otherwise an error.
+func ByteArray2ReponseWriter(file http.ResponseWriter, bytes []byte) error {
 	nsum := 0
 	n := 0
 	var err error
